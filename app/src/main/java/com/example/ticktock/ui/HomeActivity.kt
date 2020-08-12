b5b39d64281b833fda29fef3ad5f5938f249e98b
luisernesto <ellokodeluis@gmail.com>
2020-08-12 18:53:00 +0200

    ui has been created

--- app/src/main/java/com/example/ticktock/ui/HomeActivity.kt
+++ app/src/main/java/com/example/ticktock/ui/HomeActivity.kt
@@ -1,11 +1,14 @@
-package com.example.ticktock
+package com.example.ticktock.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ticktock.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
