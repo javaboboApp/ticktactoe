5bb8748359b46a64441371cf4aecb628b82c7ea6
luisernesto <ellokodeluis@gmail.com>
2020-08-13 09:27:09 +0200

    another test in board has been implemented

--- app/src/test/java/com/example/ticktock/models/Board/BoardTest.kt
+++ app/src/test/java/com/example/ticktock/models/Board/BoardTest.kt
@@ -26,6 +26,19 @@ class BoardTest{
         board.move(ROWS, COLUMMS, PLAYER_1)
     }
 
+    @Test
+    fun test_board_does_not_throw_exeptions() {
+        val board = Board()
+        board.move(0, 1, PLAYER_1)
+        board.move(0, 2, PLAYER_1)
+        board.move(0, 3, PLAYER_1)
 
+        board.move(1, 0, PLAYER_1)
+        board.move(1, 1, PLAYER_1)
+        board.move(1, 2, PLAYER_1)
 
+        board.move(2, 0, PLAYER_1)
+        board.move(2, 1, PLAYER_1)
+        board.move(2, 2, PLAYER_1)
+    }
 }
