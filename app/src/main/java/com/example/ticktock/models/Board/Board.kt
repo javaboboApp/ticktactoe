81a9f7e52bff1fc9d24b4e1b7068fdeb97b8d28e
luisernesto <ellokodeluis@gmail.com>
2020-08-14 04:28:12 +0200

    bug has been fixed

--- app/src/main/java/com/example/ticktock/models/Board/Board.kt
+++ app/src/main/java/com/example/ticktock/models/Board/Board.kt
@@ -88,7 +88,8 @@ class Board() : IBoard {
 
                 (matrix[0][2] == matrix[1][2] && matrix[0][2] == matrix[2][2]) ||
                 (matrix[0][1] == matrix[1][1] && matrix[0][1] == matrix[2][1]) ||
-                (matrix[1][0] == matrix[1][1] && matrix[1][0] == matrix[1][2])
+                (matrix[1][0] == matrix[1][1] && matrix[1][0] == matrix[1][2]) ||
+                (matrix[2][0] == matrix[1][1] && matrix[2][0] == matrix[0][2])
     }
 
 
